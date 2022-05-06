5666eeaa1d97f26179a4a230533117c95f0a106d
mosheurpau <mosheur.pau@gamil.com>
2022-05-06 12:51:59 +0600

    setup express server with dotenv

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Furniture warehouse server");
