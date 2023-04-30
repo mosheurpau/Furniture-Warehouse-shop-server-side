80082a33dad4db987a910c98b9417afd93edea49
mosheurpau <mosheur.pau@gamil.com>
2023-05-01 04:09:25 +0600

    change API hosting

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// mongodb://localhost:27017
+
+// const uri = `mongodb://localhost:27017`;
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qwxmj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
